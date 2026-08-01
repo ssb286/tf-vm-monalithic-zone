839c76e9f3cf09cdd0a5a3b142cf1dcb0704cba7
ssb286 <abc@gmail.com>
2026-08-01 15:59:11 +0530

    test2-added-rg

--- root/provider.tf
+++ root/provider.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "Backend_Storage"
-    storage_account_name = "gittestnoida"           # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "gitpulltest"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "sarv.terrafrom.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "Backend_Storage"
+  #   storage_account_name = "gittestnoida"           # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  #   container_name       = "gitpulltest"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  #   key                  = "sarv.terrafrom.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  # }
 }
 
 provider "azurerm" {
